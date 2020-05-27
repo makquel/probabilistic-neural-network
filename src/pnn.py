bc3f8357fcaf4e1d7ac4228964eef8001885f57a
Miguel Rueda <makquel@gmail.com>
2020-05-27 16:12:05 -0300

    testing notebooks and pnn class file

---
+++ src/pnn.py
@@ -0,0 +1,74 @@
+class PNN:
+    def __init__(self, sigma):
+        self.sigma = sigma
+
+    def __repr__(self):
+        return 'Sigma(%r)' % self.sigma
+    
+    def fit(self,X_train,X_test,y_train):
+        '''
+        
+        '''
+        # n_classes = len(my_data.groupby('<class_name_space>''))
+        ## TODO: improve the number of classes value
+        self.X_train = X_train
+        self.y_train = y_train
+        self.classes = max(y_train)        
+
+                
+    def score(self, X_test,y_test):
+        '''
+        
+        '''
+        sigma = self.sigma
+        n_classes = self.classes
+        X_train = self.X_train
+        y_train = self.y_train
+        prob = np.zeros((X_test.shape[0],max(y_train))) 
+        
+        ## loop through all the X_test data (unclassified points)
+        for point in range(0,X_test.shape[0]):
+            x_test = X_test[point,:] 
+        #     print(x_test)
+            ## loop through i-th class
+            for i in range(1,n_classes+1):
+                X_i = X_train[np.where(y_train == i)[0],:]
+                summ = 0.
+                ## loop through j-th element of the i-th class
+                for j in range(0,X_i.shape[0]):
+                    ## norm L2 (sum of squares)
+                    norm = 0.
+                    ## loop through k-th feature
+                    for k in range(0,X_i.shape[1]):
+                        ## TODO: covariance form X*M*X'
+                        norm = norm + (X_i[j,k]-x_test[k])*(X_i[j,k]-x_test[k])  
+                    ## Summation of Gaussians
+                    summ = summ + np.exp((-1/2)*(1/(2*sigma**2))*norm)
+                ## Average of Parzen Window (normalization term)
+                summ = summ/X_i.shape[0]
+                ## Decision boundary
+                prob[point, (i-1)] = summ
+                self.prob = prob
+                
+        y_prob = np.asarray([max(self.prob[i,:]) for i in range(0,self.prob.shape[0])])
+        self.y_bar = np.asarray([np.where(prob[ix,:] == max(prob[ix,:]))[0][0]+1 for ix in range(0,prob.shape[0])])
+        acc_score = accuracy_score(y_test, self.y_bar)
+        
+        return acc_score
+    
+
+    def predict(self,y_test):
+        ## TODO: 
+        # argmax of each 
+#         prob = self.prob
+        y_prob = np.asarray([max(self.prob[i,:]) for i in range(0,self.prob.shape[0])])
+        self.y_bar = np.asarray([np.where(prob[ix,:] == max(prob[ix,:]))[0][0]+1 for ix in range(0,prob.shape[0])])
+#         self.y_bar = y_bar
+#         return self.y_bar
+        
+    
+    def predict_proba(self,X_range):
+#         self.fit(self.X_train,X_range,self.y_train)
+        acc_tmp = self.score(X_range,self.y_train)
+        
+        return self.prob
